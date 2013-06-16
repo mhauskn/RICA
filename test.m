ecae007444be879b9bde2fc0ea00971e2bb5bcd1
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2013-06-16 18:29:31 -0500

    Verified that the matrix form of the data was equivalent to summing the gradients.

--- test.m
+++ test.m
@@ -31,7 +31,7 @@ end
 theta = theta';
 
 W = reshape(theta, layersizes(2), layersizes(1));
-x = rand(layersizes(1),1);
+x = rand(layersizes(1),5);
 
 [cost, grad] = rica(W, layersizes, x)
 
@@ -40,28 +40,30 @@ x = rand(layersizes(1),1);
 % derv = 2*(H(1) - x(1))*(2*x(1)*W(1,1)+W(1,2)*x(2)) + 2*(H(2)-x(2))*(W(1,2)*x(1))
 
 % Compute an emperical derivate of the function
+% This is equivalent to going column-wise through 
+% x and summing the derivatives
 empGrad = zeros(layersizes(2), layersizes(1));
 for row = 1 : size(grad)(1),
   for col = 1 : size(grad)(2),
     init = W(row,col);
-    dx = 1;
-    for i=1:10,
-      W(row,col) = init + dx;
-      h = W*x;
-      H = W'*h;
-      a = H - x;
-      high = sum(a(:).^2);
 
-      W(row,col) = init - dx;
-      h = W*x;
-      H = W'*h;
-      a = H - x;
-      low = sum(a(:).^2);
+    dx = 1e-10;
+    W(row,col) = init + dx;
+    h = W*x;
+    H = W'*h;
+    a = H - x;
+    high = sum(a(:).^2);
 
-      newDerv = (high - low) / (2 * dx);
-      dx = dx / 2;
-    end
-    empGrad(row,col) = newDerv;
+    W(row,col) = init - dx;
+    h = W*x;
+    H = W'*h;
+    a = H - x;
+    low = sum(a(:).^2);
+
+    newDerv = (high - low) / (2 * dx);
+    dx = dx / 2;
+
+    empGrad(row,col) += newDerv;
     W(row,col) = init;
   end
 end
