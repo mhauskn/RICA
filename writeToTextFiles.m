de0f3f60acf4fca127d77ba102a6dacfaad3bd71
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2013-07-26 14:06:30 -0500

    Fixed bug in deepAutoencoder that caused gradients to be too large.

--- writeToTextFiles.m
+++ writeToTextFiles.m
@@ -10,18 +10,14 @@ for i=1:l-1
     W{i} = reshape(theta(lold:lnew), layersizes(i+1), layersizes(i));
     
     dlmwrite(['W', num2str(i), '.txt'], W{i});
-    lold = lnew + 1;
-    lnew = lnew + layersizes(i+1);
-    b{i} = theta(lold:lnew);
-    dlmwrite(['b', num2str(i), '.txt'], b{i});
 end
 % handle tied-weight stuff
-j = 1;
-for i=l:2*(l-1)
-    lold = lnew + 1;
-    lnew = lnew + layersizes(l-j);    
-    b{i} = theta(lold:lnew);
-    dlmwrite(['b', num2str(i), '.txt'], b{i});
-    j = j + 1;
-end
+% j = 1;
+% for i=l:2*(l-1)
+%     lold = lnew + 1;
+%     lnew = lnew + layersizes(l-j);    
+%     b{i} = theta(lold:lnew);
+%     dlmwrite(['b', num2str(i), '.txt'], b{i});
+%     j = j + 1;
+% end
 assert(lnew == length(theta), 'Error: dimension does not match\n')
